1d74637225604468ec7cde56f2657a3f2d735abb
lzgirlcat <lz@girlc.at>
2026-09-25 17:32:09 +0200

    move to swaymsg from wmctl

--- src/display_info.rs
+++ src/display_info.rs
@@ -6,24 +6,27 @@ use eyre::{Context, Result};
 pub struct DisplayInfo {
     pub model: String,
     pub name: String,
-    pub description: String,
+    pub serial: String,
+    pub make: String,
 }
 
 impl DisplayInfo {
     pub fn get_displays() -> Result<Vec<Self>> {
         let outputs = String::from_utf8(
-            Command::new("wmctl")
-                .args(["list-outputs", "--json"])
+            Command::new("swaymsg")
+                .args(["-t", "get_outputs", "--raw"])
                 .output()?
                 .stdout,
         )?;
-        serde_json::from_str(&outputs).context("failed to parse wmctl output")
+        serde_json::from_str(&outputs).context("failed to swaymsg output")
     }
 
     /// Match the display name against the display's model name, id or description
     pub fn match_name(&self, display_name: &str) -> bool {
         self.name.contains(display_name)
             || self.model.contains(display_name)
-            || self.description.contains(display_name)
+            || self.serial.contains(display_name)
+            || self.make.contains(display_name)
+            || format!("{} {} {}", self.make, self.model, self.serial) == display_name
     }
 }
